3f9f934a8c49bcbae0cb49063a44997e0aea8cc2
RussianBoy64 <kozlachkovvd@gmail.com>
2022-12-10 18:40:22 +0400

    feat: full type coverage for sources.ts

--- news-JS/src/components/view/sources/sources.ts
+++ news-JS/src/components/view/sources/sources.ts
@@ -2,11 +2,11 @@ import './sources.css';
 import { IArticleSouce } from '../../../types/interfaces';
 
 class Sources {
-  draw(data: IArticleSouce[]) {
+  public draw(data: Array<IArticleSouce>) {
     const fragment = document.createDocumentFragment();
     const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp')!;
 
-    data.forEach((item) => {
+    data.forEach((item: IArticleSouce) => {
       const sourceClone = sourceItemTemp!.content.cloneNode(true) as Element;
 
       sourceClone.querySelector('.source__item-name')!.textContent = item.name;
