149dc4d5d2907fb334be945fd7a95b95d7d8f445
RussianBoy64 <kozlachkovvd@gmail.com>
2023-01-18 00:03:02 +0400

    feat: add fonts and styles for NavLinks

--- async-race/src/components/Header/index.tsx
+++ async-race/src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
 import routes from "../../routes";
+import styles from "./styles.module.scss";
 
 function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       {routes.map((route) => (
-        <NavLink to={route.path}>{route.page}</NavLink>
+        <NavLink className={styles.link} key={route.id} to={route.path}>
+          {route.page}
+        </NavLink>
       ))}
     </header>
   );
